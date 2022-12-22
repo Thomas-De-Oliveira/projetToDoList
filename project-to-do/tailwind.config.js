b56108772a2f91b5cb4a2d0d154e0c34906e8581
Thomas <thomas.deoliveira@1health.fr>
2022-12-22 11:42:10 +0100

    Progression dans l'installation

--- project-to-do/tailwind.config.js
+++ project-to-do/tailwind.config.js
@@ -1,6 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/pages/**/*.{js,jsx}",
+    "./src/components/**/*.{js,jsx}",
+  ],
   theme: {
     extend: {},
   },
